31156b3efa712335d7814da470cbba5191fb03a1
Lucas <lucas.tabis@epitech.eu>
2020-01-28 16:41:07 +0100

    :sparkles: Data format and module interface.

---
+++ Data.hpp
@@ -0,0 +1,76 @@
+/*
+** EPITECH PROJECT, 2020
+** Zia
+** File description:
+** Data for communication between modules
+*/
+
+#pragma once
+
+#include <unordered_map>
+#include <string>
+
+/// \namespace Zia
+/// \brief The Zia web server namespace.
+namespace Zia
+{
+    /// \struct Request
+    /// \brief Structure containing request informations
+    struct Request
+    {
+        /*! String containing the method of the request (GET, POST ...) */
+		std::string method;
+
+		/*! String containing the uri of the request */
+		std::string requesturi;
+
+		/*! String containing the http version */
+		std::string http_version;
+
+        /*! map of all headers in the request associated with their values respectively */
+        std::unordered_map<std::string, std::string> headers;
+
+        /*! String containing only the content of the request's body */
+        std::string body_content;
+
+        /*! String containing the request as it is received */
+        std::string raw_resquest;
+    };
+
+    /// \struct Response
+    /// \brief Structure containing response informations
+    struct Response
+    { 
+        /* String containing the http version of the response */
+		std::string http_version;
+
+		/* String containing the response's status code */
+		std::string status_code;
+
+		/* String containing the response's reason phrase */
+		std::string reason_phrase;
+
+        /* Map containing all headers of the response */
+        std::unordered_map<std::string, std::string> headers;
+
+        /* String containing the response body */
+        std::string body_content;
+
+        /* String containing the current type of the response (ex: py, php ...) */
+        std::string type;
+
+        /*! String containing the respond as it will be send */ 
+        std::string raw_response;
+    };
+
+    /// \struct Data
+    /// \brief Structure containing request and response informations
+    struct Data
+    {
+        /* Structure containing request informations */
+        Request request;
+
+        /* Structure containing response informations */
+        Response response;
+    };
+}
---
+++ IModule.hpp
@@ -0,0 +1,53 @@
+/*
+** EPITECH PROJECT, 2019
+** IModule.hpp
+** File description:
+** Module interface
+*/
+
+#pragma once
+
+#include <string>
+
+#include "Data.hpp"
+
+/// \namespace Zia
+/// \brief The Zia web server namespace.
+namespace Zia
+{
+	/// \enum ModuleStatus
+	/// \brief enum with all status possible for a module
+	enum ModuleStatus {
+		Done, Error, Processing
+	};
+
+	/// \class IModule
+	/// \brief An interface for modules, wich is our API to interact with the Application server.
+	class IModule
+	{
+	public:
+
+		/// \brief get the name of the current module.
+		/// \return the name of the module.
+		virtual inline std::string name() const = 0;
+		/// \brief get the priority of the current module.
+		/// \return the priority of the module.
+		virtual inline std::size_t priority() const = 0;
+		/// \brief get the status of the module.
+		/// \return status of the module
+		virtual inline ModuleStatus getStatus() const = 0;
+
+		/// \brief unload the current module.
+		virtual void unloadModule() = 0;
+		/// \brief load the current module.
+		/// \param priority order priority of the module.
+		virtual void loadModule(std::size_t priority) = 0;
+		/// \brief set the priority of the current module.
+		/// \param priority : the priority to assign to current the module.
+		virtual inline void setPriority(std::size_t priority) = 0;
+
+		/// \brief process the data.
+		/// \param data structure with informations about the resquest and the response.
+		virtual void processData(Data &data) = 0;
+	};
+}
